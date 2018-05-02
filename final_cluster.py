3c5db40bc0085b8207f262b3810606f28f15a91c
Kenny  Song <kennysong@wireless-nat-inside.uiowa.edu>
2018-05-02 16:00:40 -0500

    clean up

--- final_cluster.py
+++ final_cluster.py
@@ -27,25 +27,6 @@ def cluster(curUVs, numClusts, userIds):
     rowHeaders = []
     dataMatrix = []
 
-    # for user in curUVs:
-    #     data = line.strip().split("|")
-    #     rowHeaders.append(curUVs)
-    #     dataMatrix.append([float(x) for x in data[1:]])
-
-    # for userId, ratings in curUVs.iteritems():
-    #     rowHeaders.append(userId)
-    #     dataMatrix.append(ratings)
-
-    #convert native data array into a numpy array
-    print 'begin'
-    #dataMatrix = np.array(dataMatrix)
-    print 'here'
-    #distanceMatrix = pdist(dataMatrix,'euclidean')
-    print 'here2'
-    #linkageMatrix = linkage(distanceMatrix)
-    print 'here3'
-    #Xclust = hierarchy.fcluster(linkageMatrix, numClusts, 'maxclust')
-    print 'here4'
     Xclust = hierarchy.fclusterdata(curUVs, numClusts, 'maxclust')
 
     clusters = {}
@@ -119,127 +100,111 @@ def main():
 
     fold_name = ""
     #' + str(fold_num) + '
+    for foldIndex in range(0,10):
+        fold_file = "folds_" + str(fold_num) + "/review_fold_" + str(foldIndex) + ".txt"
 
-    fold_file = "folds_" + str(fold_num) + "/review_fold_0.txt"
-
-    curUVs = userVectors
+        curUVs = userVectors
 
 
-    test = ['aa','bb','cc']
-    # print 'range'
-    # print range(test)
+        test = ['aa','bb','cc']
+        # print 'range'
+        # print range(test)
 
-    userPredictions = {}
-    f = open(fold_file, 'r')
-    for userLine in f: #current fold
-        user = userLine.strip().split("|")
-        user = user.pop(0)
-        listOfRatedMovies = []
-        userPredictions[user] = []
-        #print curUVs[user]
-        movieIndex = 0
-        # print 'users----'
-        # print curUVs[user]
-        # print 'users----'
-        for movie in curUVs[user]:
-            if(movie != 0.0):
-                #if movieIndex in listOfRatedMovies != True:
-                listOfRatedMovies.append(movieIndex)
-            movieIndex = movieIndex + 1
+        userPredictions = {}
+        f = open(fold_file, 'r')
+        for userLine in f: #current fold
+            user = userLine.strip().split("|")
+            user = user.pop(0)
+            listOfRatedMovies = []
+            userPredictions[user] = []
+            #print curUVs[user]
+            movieIndex = 0
+      
+            for movie in curUVs[user]:
+                if(movie != 0.0):
+                    #if movieIndex in listOfRatedMovies != True:
+                    listOfRatedMovies.append(movieIndex)
+                movieIndex = movieIndex + 1
 
-        #print listOfRatedMovies
-        #choose random movie to remove and note position of movie and original rating                           
-        removedMovie = random.choice(listOfRatedMovies)
+            #print listOfRatedMovies
+            #choose random movie to remove and note position of movie and original rating                           
+            removedMovie = random.choice(listOfRatedMovies)
 
-        #print removedMovie
-        userPredictions[user].append(removedMovie) #the movie index
-        userPredictions[user].append(curUVs[user][removedMovie]) #movie value
-        ##print userPredictions[user][0]
-        #print listOfRatedMovies
-        #Once recorded, remove movie from test vectors
-        curUVs[user][removedMovie] = 0.0
+            #print removedMovie
+            userPredictions[user].append(removedMovie) #the movie index
+            userPredictions[user].append(curUVs[user][removedMovie]) #movie value
+          
+            #Once recorded, remove movie from test vectors
+            curUVs[user][removedMovie] = 0.0
 
-    
-    userIds = []
-    vectors = []
-    for userId, ratings in curUVs.iteritems():
-        userIds.append(userId)
-        vectors.append(ratings)   
+        
+        userIds = []
+        vectors = []
+        for userId, ratings in curUVs.iteritems():
+            userIds.append(userId)
+            vectors.append(ratings)   
 
-    # # vectorFile = open(sys.argv[1], 'r')
-    # vectors = []
-    # userIds = []
-    # for line in vectorFile.readlines()[1:]:
-    #     userIds.append(line.split('|')[0].strip())
-    #     tmpVector = line.split('|')[1:]
-    #     curVector = [float(x) for x in tmpVector]
-    #     vectors.append(curVector)
 
-    #print vectors
-    # # #Hierarchal clustering with the total number of users/10 for the number of clusters
-    numClusts = len(userIds)/100
+        # # #Hierarchal clustering with the total number of users/10 for the number of clusters
+        numClusts = len(userIds)/100
 
 
 
-    cluster_results,user_cluster = cluster(vectors, numClusts, userIds)
-    print "done with clustering"
+        cluster_results,user_cluster = cluster(vectors, numClusts, userIds)
+        #print "done with clustering"
 
-    foldFile = open(fold_file, 'r')
+        foldFile = open(fold_file, 'r')
 
 
-    for userLine in foldFile: 
-     
-        user = userLine.strip().split("|")
-        user = user.pop(0)
-        movieNeedingPrediction = userPredictions[user][0] #movieIndex
+        for userLine in foldFile: 
+         
+            user = userLine.strip().split("|")
+            user = user.pop(0)
+            movieNeedingPrediction = userPredictions[user][0] #movieIndex
 
-        userGroupClusterIndex = 0
-        #theUserGroup = []
-        finalCounter = 0
+            userGroupClusterIndex = 0
+            #theUserGroup = []
+            finalCounter = 0
 
-        clusterUsersIds = cluster_results[user_cluster[user]]
+            clusterUsersIds = cluster_results[user_cluster[user]]
 
-        clusteredRatings = []
-        #print theUserGroup
-        # for userGroupId in theUserGroup:
-        #     for userId, ratings in curUVs.iteritems():
-        #         if(userId == userGroupId):
+            clusteredRatings = []
 
-        for gUser in clusterUsersIds:            
-            needMovieRating = curUVs[gUser][movieNeedingPrediction]
-            if(needMovieRating > 0):
-                clusteredRatings.append(needMovieRating)
 
-        if len(clusteredRatings) == 0:
-            clusterId = user_cluster[user]
-            surroundingClusters = cluster_results[clusterId] 
-            for x in range(0,5):
-                surroundingCluster = cluster_results[clusterId + x]
-                for scUser in surroundingCluster:
-                    needMovieRating = curUVs[scUser][movieNeedingPrediction]
-                    if(needMovieRating > 0):
-                        clusteredRatings.append(needMovieRating)
+            for gUser in clusterUsersIds:            
+                needMovieRating = curUVs[gUser][movieNeedingPrediction]
+                if(needMovieRating > 0):
+                    clusteredRatings.append(needMovieRating)
 
             if len(clusteredRatings) == 0:
+                clusterId = user_cluster[user]
+                surroundingClusters = cluster_results[clusterId] 
                 for x in range(0,5):
-                    if(clusterId > 5):
-                        surroundingCluster = cluster_results[clusterId - x]
-                        for scUser in surroundingCluster:
-                            needMovieRating = curUVs[scUser][movieNeedingPrediction]
-                            if(needMovieRating > 0):
-                                clusteredRatings.append(needMovieRating)
+                    surroundingCluster = cluster_results[clusterId + x]
+                    for scUser in surroundingCluster:
+                        needMovieRating = curUVs[scUser][movieNeedingPrediction]
+                        if(needMovieRating > 0):
+                            clusteredRatings.append(needMovieRating)
+
+                if len(clusteredRatings) == 0:
+                    for x in range(0,5):
+                        if(clusterId > 5):
+                            surroundingCluster = cluster_results[clusterId - x]
+                            for scUser in surroundingCluster:
+                                needMovieRating = curUVs[scUser][movieNeedingPrediction]
+                                if(needMovieRating > 0):
+                                    clusteredRatings.append(needMovieRating)
 
 
-        predictedRating = sum(clusteredRatings)/len(clusteredRatings)
-        #print predictedRating
-        userPredictions[user].append(predictedRating)
-        #print userPredictions[user]
+            predictedRating = sum(clusteredRatings)/len(clusteredRatings)
 
-# #     spread = 10 #indexes we'll look at in either direction of the movie needing prediction
+            userPredictions[user].append(predictedRating)
 
 
-    for final in userPredictions:
-            print final + "|" + movies[userPredictions[final][0]] + "|" + str(userPredictions[final][1])+ "|" + str(userPredictions[final][2])
+        print fold_file    
+
+        for final in userPredictions:
+                print final + "|" + movies[userPredictions[final][0]] + "|" + str(userPredictions[final][1])+ "|" + str(userPredictions[final][2])
 
 
 
