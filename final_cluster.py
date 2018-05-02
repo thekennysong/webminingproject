b593790b000f6e8ca515b4787b3e33ec7bb0d733
Kenny  Song <kennysong@wireless-nat-inside.uiowa.edu>
2018-05-02 15:57:24 -0500

    WOrking

--- final_cluster.py
+++ final_cluster.py
@@ -13,13 +13,13 @@ from pylab import plot,show
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage, leaves_list
 import numpy as np
-from scipy.cluster.hierarchy import cophenet
 from scipy.spatial.distance import pdist
 import sys
 import os
 import random
+from scipy.cluster import hierarchy
 
-def cluster(curUVs):
+def cluster(curUVs, numClusts, userIds):
     #open the file assuming the data above is in a file called 'dataFile'
     #inFile = open(sys.argv[1],'r')
     #save the column/row headers (conditions/genes) into an array
@@ -32,40 +32,33 @@ def cluster(curUVs):
     #     rowHeaders.append(curUVs)
     #     dataMatrix.append([float(x) for x in data[1:]])
 
-    for userId, ratings in curUVs.iteritems():
-        rowHeaders.append(userId)
-        dataMatrix.append(ratings)
+    # for userId, ratings in curUVs.iteritems():
+    #     rowHeaders.append(userId)
+    #     dataMatrix.append(ratings)
 
     #convert native data array into a numpy array
-    dataMatrix = np.array(dataMatrix)
-    distanceMatrix = pdist(dataMatrix,'euclidean')
-    linkageMatrix = linkage(distanceMatrix)
-    
-    heatmapOrder = leaves_list(linkageMatrix)
+    print 'begin'
+    #dataMatrix = np.array(dataMatrix)
+    print 'here'
+    #distanceMatrix = pdist(dataMatrix,'euclidean')
+    print 'here2'
+    #linkageMatrix = linkage(distanceMatrix)
+    print 'here3'
+    #Xclust = hierarchy.fcluster(linkageMatrix, numClusts, 'maxclust')
+    print 'here4'
+    Xclust = hierarchy.fclusterdata(curUVs, numClusts, 'maxclust')
 
-    orderedDataMatrix = dataMatrix[heatmapOrder,:]
-    rowHeaders = np.array(rowHeaders)
-    orderedRowHeaders = rowHeaders[heatmapOrder]
+    clusters = {}
+    userCluster = {}
 
-    #output data for visualization in a browser with javascript/d3.js
-    matrixOutput = []
-    row = 0
-    for rowData in orderedDataMatrix:
-        col = 0
-        rowOutput = []
-        for colData in rowData:
-            rowOutput.append([colData, row, col])
-            col += 1
-        matrixOutput.append(rowOutput)
-        row += 1
+    for i in range(len(Xclust)):
+        if Xclust[i] not in clusters:
+            clusters[int(Xclust[i])] = []
+        clusters[int(Xclust[i])].append(userIds[i])
+        userCluster[userIds[i]] = int(Xclust[i])
 
-    print 'var maxData = ' + str(np.amax(dataMatrix)) + ";"
-    print 'var minData = ' + str(np.amin(dataMatrix)) + ";"
-    print 'var data = ' + str(matrixOutput) + ";"
-    print 'var cols = ' + str(colHeaders) + ";"
-    print 'var rows = ' + str([x for x in orderedRowHeaders]) + ";"
 
-    return (matrixOutput, [x for x in orderedRowHeaders])
+    return clusters,userCluster
 
 def partition(k, file):
     k = int(k)
@@ -90,6 +83,8 @@ def partition(k, file):
             c += 1
 
 def main():
+
+
 
     userVectors = {}
     movies = []
@@ -148,72 +143,103 @@ def main():
         # print 'users----'
         for movie in curUVs[user]:
             if(movie != 0.0):
+                #if movieIndex in listOfRatedMovies != True:
                 listOfRatedMovies.append(movieIndex)
             movieIndex = movieIndex + 1
 
         #print listOfRatedMovies
         #choose random movie to remove and note position of movie and original rating                           
         removedMovie = random.choice(listOfRatedMovies)
+
         #print removedMovie
         userPredictions[user].append(removedMovie) #the movie index
         userPredictions[user].append(curUVs[user][removedMovie]) #movie value
-
+        ##print userPredictions[user][0]
+        #print listOfRatedMovies
         #Once recorded, remove movie from test vectors
         curUVs[user][removedMovie] = 0.0
 
-    cluster_results = cluster(curUVs)
-
-
-    print '/*user predictions--------'
-    print userPredictions
-    print 'user predictions--------*/'
-
-    # print 'cluster results---------'
-    # print cluster_results
-    # print 'cluster results---------'
-    #     print curUVs
-
-    cluster_nums = cluster_results[0]   
-    cluster_ids = cluster_results[1] 
-
     
+    userIds = []
+    vectors = []
+    for userId, ratings in curUVs.iteritems():
+        userIds.append(userId)
+        vectors.append(ratings)   
 
+    # # vectorFile = open(sys.argv[1], 'r')
+    # vectors = []
+    # userIds = []
+    # for line in vectorFile.readlines()[1:]:
+    #     userIds.append(line.split('|')[0].strip())
+    #     tmpVector = line.split('|')[1:]
+    #     curVector = [float(x) for x in tmpVector]
+    #     vectors.append(curVector)
+
+    #print vectors
+    # # #Hierarchal clustering with the total number of users/10 for the number of clusters
+    numClusts = len(userIds)/100
+
+
+
+    cluster_results,user_cluster = cluster(vectors, numClusts, userIds)
+    print "done with clustering"
 
     foldFile = open(fold_file, 'r')
+
+
     for userLine in foldFile: 
+     
         user = userLine.strip().split("|")
         user = user.pop(0)
         movieNeedingPrediction = userPredictions[user][0] #movieIndex
 
+        userGroupClusterIndex = 0
+        #theUserGroup = []
         finalCounter = 0
-        for userId in cluster_ids:
-            if user == userId:
-                goldenIndex = finalCounter
 
-            finalCounter = finalCounter + 1
+        clusterUsersIds = cluster_results[user_cluster[user]]
 
-        spread = 10 #indexes we'll look at in either direction of the movie direction
+        clusteredRatings = []
+        #print theUserGroup
+        # for userGroupId in theUserGroup:
+        #     for userId, ratings in curUVs.iteritems():
+        #         if(userId == userGroupId):
+
+        for gUser in clusterUsersIds:            
+            needMovieRating = curUVs[gUser][movieNeedingPrediction]
+            if(needMovieRating > 0):
+                clusteredRatings.append(needMovieRating)
+
+        if len(clusteredRatings) == 0:
+            clusterId = user_cluster[user]
+            surroundingClusters = cluster_results[clusterId] 
+            for x in range(0,5):
+                surroundingCluster = cluster_results[clusterId + x]
+                for scUser in surroundingCluster:
+                    needMovieRating = curUVs[scUser][movieNeedingPrediction]
+                    if(needMovieRating > 0):
+                        clusteredRatings.append(needMovieRating)
+
+            if len(clusteredRatings) == 0:
+                for x in range(0,5):
+                    if(clusterId > 5):
+                        surroundingCluster = cluster_results[clusterId - x]
+                        for scUser in surroundingCluster:
+                            needMovieRating = curUVs[scUser][movieNeedingPrediction]
+                            if(needMovieRating > 0):
+                                clusteredRatings.append(needMovieRating)
 
 
+        predictedRating = sum(clusteredRatings)/len(clusteredRatings)
+        #print predictedRating
+        userPredictions[user].append(predictedRating)
+        #print userPredictions[user]
+
+# #     spread = 10 #indexes we'll look at in either direction of the movie needing prediction
 
 
-
-        #predictedRating = 
-        #print movieNeedingPrediction
-
-
-
-
-    # print userVectors
-    # print rowHeaders
-    # print dataMatrix
-    # print movies
-
-    
-   
-    #print ', '.join(rowHeaders)
-   # print ', '.join(dataMatrix)
-
+    for final in userPredictions:
+            print final + "|" + movies[userPredictions[final][0]] + "|" + str(userPredictions[final][1])+ "|" + str(userPredictions[final][2])
 
 
 
